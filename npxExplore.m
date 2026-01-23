13bbc89732d4c956af79aa4a3b0ced064c9be32f
Chris Klink <p.c.klink@gmail.com>
2026-01-23 18:05:18 +0100

    wip

--- npxExplore.m
+++ npxExplore.m
@@ -13,24 +13,25 @@ else
 end
 
 subject = 'm032';
-day = '20251022';
-expt = 1; rec = 1;
+day = '20240826';
+expt = 1; reci = 1;
 ProbeLabel = 'Record Node 101';
 
 %% re-order data for the tools to work
 data_root_org = fullfile(data_fld,subject,day);
 data_root_probe = fullfile(data_fld,subject,day,ProbeLabel);
 % put experiments under probe lable
-mkdir(data_root_probe);
-movefile(fullfile(data_root_org,'experiment*'),data_root_probe);
+if ~isdir(data_root_probe)
+    mkdir(data_root_probe);
+    movefile(fullfile(data_root_org,'experiment*'),data_root_probe);
+end
 
 % put it back
 %movefile(fullfile(data_root_probe,'experiment*'),data_root_org);
 %rmdir(data_root_probe)
+
 % add the linked folder to the path so that the toolbox can search it
 addpath(genpath(fullfile(data_root_probe,ProbeLabel)));
-
-% cannot create a symlink on the server?
 session = Session(fullfile(data_fld,subject,day));
 % session now contain the recording info
 
@@ -40,7 +41,7 @@ node = session.recordNodes{1};
 % each entry of node.recordings{} is a a run 
 % note that runs get indexed in ascending order; use the filepath under
 % node.recordings{i}.directory to identify the real run number
-rec = node.recordings{2}; % refers to a run/recording
+rec = node.recordings{reci}; % refers to a run/recording
 disp(rec); % see info
 
 % identify what the datastreams are
@@ -60,12 +61,12 @@ BNCstream = rec.continuous(cskeys{1});
 APstream = rec.continuous(cskeys{2});
 
 %% convert timebase
-trange=(1:1e5);
 achan = 5;
 ts = 0:1/BNCstream.metadata.sampleRate:...
-    (length(BNCstream.timestamps)/BNCstream.metadata.sampleRate)-1;
+    length(BNCstream.timestamps)*(1/BNCstream.metadata.sampleRate);
+ts(end)=[];
 ts2 = BNCstream.timestamps - BNCstream.timestamps(1);
-%plot(ts2(trange),BNCstream.samples(achan,trange))
+%trange=(1:1e5);plot(ts2(trange),BNCstream.samples(achan,trange))
 
 d=BNCstream.samples(achan,:);
 dd = abs(diff(d));
@@ -86,13 +87,6 @@ idx = [dd6kidx(1) dd6kidx(tsd>0.500)];
 % xline(ts3(idx),'r')
 % yline(6000,'g')
 
-%% check what info is available on external input
-% figure;
-% for i=1:8
-%     subplot(8,1,i);
-%     plot(BNCstream.samples(i,:))
-% end
-
 %% check what info is available as events
 % looked at XL's scripts
 disp 'Events'
@@ -107,12 +101,13 @@ bits = {D.line double(D.timestamp)-BNCstream.timestamps(1) ...
 % trial bit = 4
 
 TrOn = bits{2}(bits{1}==4 & bits{4}==1); % trial
-b8 = bits{2}(bits{1}==8  & bits{4}==1); % correct
+b8 = bits{2}(bits{1}==8  & bits{4}==1); % reward
 b1 = bits{2}(bits{1}==1  & bits{4}==1);
 
 %TrC = bits{2}(bits{1}==6);
 
-figure; hold on;
+figure; 
+subplot(2,1,1); hold on;
 plot(ts3,dd,'Color',[0.2 0.2 0.2])
 xline(ts3(idx),'r','LineWidth',3)
 yline(6000,'g','LineWidth',1)
@@ -123,32 +118,58 @@ xline(b8,'m','LineWidth',1)
 set(gca,'xlim',[0 30]);
 
 %% get the Tracker log
-load(fullfile(data_fld,subject,day,'run-006_T-143435',...
-    'sub-m032_ses-20251022_task-ct_stim-fgdots_run-006'));
+% load(fullfile(data_fld,subject,day,'run-006_T-143435',...
+%     'sub-m032_ses-20251022_task-ct_stim-fgdots_run-006'));
+load(fullfile(data_fld,subject,day,ProbeLabel,'experiment1',...
+    'recording3','run-003_T-154348',...
+    'sub-m032_ses-20240826_task-ct_stim-fgdots_run-003'));
 %%
-TrLog = [];
+TrLog = []; toffset = 0;
+MotionLog = []; DelayLog = [];
+ITI0Log = []; ITI1Log = [];
+TargLog = []; CorrLog = [];
+RewStart = [];
 for i=1:length(Log.events)
     if strcmp(Log.events(i).type,'trial_start')
-        TrLog = [TrLog; Log.events(i).t];
+        TrLog = [TrLog; Log.events(i).t + toffset Log.events(i).trialn];
+        motion_logged = false;
+    elseif strcmp(Log.events(i).type,'motion_start') 
+        if ~motion_logged
+            MotionLog = [MotionLog; Log.events(i).t + toffset];
+        end
+        motion_logged = true;
+    elseif strcmp(Log.events(i).type,'delay_start') 
+        DelayLog = [DelayLog; Log.events(i).t + toffset];
+    elseif strcmp(Log.events(i).type,'reward_start') 
+        RewStart = [RewStart; Log.events(i).t + toffset];    
+    elseif strcmp(Log.events(i).type,'iti_start')
+        ITI0Log = [ITI0Log; Log.events(i).t + toffset];
+    elseif strcmp(Log.events(i).type,'iti_stop')
+        ITI1Log = [ITI1Log; Log.events(i).t + toffset];
+        toffset = toffset + Log.events(i).t;
+    elseif strcmp(Log.events(i).type,'targ_start')
+        TargLog = [TargLog; Log.events(i).t + toffset];
+    elseif strcmp(Log.events(i).type,'correct')
+        CorrLog = [CorrLog; Log.events(i).t + toffset];
     end
 end
 
-TrLog2 = TrLog-TrLog(1)+TrOn(1);
-%plot(TrLog2,10000*ones(size(TrLog2)),'ow','MarkerSize',10)
+subplot(2,1,2); hold on;
+% get timing offset OpenEphys / Tracker
+dt = TrOn(1) - TrLog(1,1);
+
+xline(TrLog(:,1)+dt,'c');
+xline(MotionLog+dt,'g');
+xline(DelayLog+dt,'c--');
+%xline(TargLog+dt,'c');
+xline(RewStart+dt,'m');
+set(gca,'xlim',[0 30]);
 
 % NB! onset times are not referenced to Experiment start
 % check runstim and correct if desired
 
-%% 
-corrdur =[];
-for i = 1:length(b8)
-    tidx = find(TrOn<b8(i),1,'last');
-    corrdur = [corrdur; b8(i)-TrOn(tidx)];
-end
-
-
 %% get the average response over all trials
-basechan = 21; numchan = 10;
+basechan = 100; numchan = 10;
 ch = basechan:basechan+numchan-1;
 tw = [-0.200 0.800];
 
@@ -170,7 +191,7 @@ Fl = 200;
 
 for i=1:size(RAW,1)
     disp(['Channel ' num2str(ch(i))])
-    S=RRAW(i,:);
+    S=RAW(i,:);
     dum1 = filtfilt(B,A,S);
     dum2 = abs(dum1);
     muafilt = filtfilt(D,C,dum2);
@@ -199,22 +220,37 @@ APd = downsample(FAP',30)';
 ns = find(APt<=1,1,'last');
 TRIALS=[];
 
-for i=1:length(TrOn)
-    tr0 = (i);
+% select correct trials
+cTrOn=[]; targtype=1;
+for r = 1:length(RewStart)
+    tRew = RewStart(r);
+    sT = find(TrLog(:,1)<tRew,1,'last');
+    cTrOn = [cTrOn;TrLog(sT,1) sT];   
+end
+tn = targidx(targidx(:,2)==targtype,1);
+scTrOn = cTrOn(ismember(cTrOn(:,2),tn),1);
+
+for i=1:length(scTrOn)
+    tr0 = scTrOn(i)+tw(1);
     si = find(APt>=tr0,1,'first');
     TRIALS = cat(3,TRIALS,APd(:,si:si+ns));
 end
 
 %size(TRIALS)
-avgT = mean(TRIALS,3);
+avgT = median(TRIALS,3);
 
 %%
 figure; hold on;
-for c= 1:length(ch)
-    plot(APt(1:1+ns)+tw(1),smooth(avgT(c,:)-mean(avgT(c,:)),50));
+for c = 1:length(ch)
+    t = APt(1:1+ns)+tw(1);
+    y = avgT(c,:);
+    tsel = t>-0.1 & t < 0;
+    yy = (y-mean(y(tsel)))./std(y(tsel));
+    plot(t,smooth(yy,33));
 end
-xline(0,'--');
-xline(0.1,'-');
+xline(0,'-');
+xline(0.1,'--');
+xline(0.2,'-');
 
 
 %% put data back in the original file structure
